de945390c744fb66733246d3591a626a071c2678
Aaron Trent <novacrazy@gmail.com>
2016-12-04 02:21:35 -0600

    Add WIP deferred lighting shaders

--- shaders/deferred_lighting.frag
+++ shaders/deferred_lighting.frag
@@ -1,17 +1,59 @@
 #version 330
 #pragma optionNV (unroll all)
+//#define DEBUG
 
 precision highp float;
+
+/* Generic reference material used with this shader:
+ * PLEASE NOTE that not all of them are correct, much to my annoyance,
+ * and the correct equations and algorithms were taken from many sources.
+ *
+ *  https://en.wikipedia.org/wiki/Specular_highlight
+ *  http://graphicrants.blogspot.com/2013/08/specular-brdf-reference.html
+ *  http://simonstechblog.blogspot.com/2011/12/microfacet-brdf.html
+ *  http://www.codinglabs.net/article_physically_based_rendering_cook_torrance.aspx
+ *  https://hal.inria.fr/hal-00942452v1/document
+ *  https://disney-animation.s3.amazonaws.com/library/s2012_pbs_disney_brdf_notes_v2.pdf
+ *  http://www.gamedev.net/topic/638197-cook-torrance-brdf-general/
+ *  http://blog.selfshadow.com/publications/s2013-shading-course/andersson/s2013_pbs_mia_notes.pdf
+ *  https://en.wikipedia.org/wiki/Lambertian_reflectance
+ *  https://en.wikipedia.org/wiki/Oren%E2%80%93Nayar_reflectance_model
+ *  https://imdoingitwrong.wordpress.com/2011/01/31/light-attenuation/
+ *  http://graphicrants.blogspot.jp/2013/12/tone-mapping.html
+ *  http://filmicgames.com/archives/75
+ *  https://github.com/cbaggers/filmic-tone-mapping-operators
+ *  https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+ *  http://blog.selfshadow.com/publications/s2013-shading-course/
+ *  http://blog.selfshadow.com/publications/s2014-shading-course/
+ *  http://blog.selfshadow.com/2014/08/14/siggraph-2014-links/
+ *  http://blog.selfshadow.com/publications/s2015-shading-course/
+ *  http://blog.selfshadow.com/publications/s2016-shading-course/
+ *  http://blog.selfshadow.com/publications/s2016-shading-course/hoffman/s2016_pbs_recent_advances_v2.pdf
+ *  http://jcgt.org/published/0003/02/03/
+ *  https://mouaif.wordpress.com/2009/01/05/photoshop-math-with-glsl-shaders/
+ *  http://developer.amd.com/wordpress/media/2012/10/ShaderX_PerPixelAniso.pdf
+ *
+ * Videos/Presentations:
+ *  https://www.youtube.com/watch?v=j-A0mwsJRmk
+ *  https://youtu.be/zs0oYjwjNEo?t=14m45s
+ *  http://www.gdcvault.com/play/1023519/Fast-Flexible-Physically-Based-Volumetric
+ *  http://www.gdcvault.com/play/1012351/Uncharted-2-HDR
+ */
+
+uniform vec3 view_position;
+uniform mat4 view;
+uniform mat4 projection;
+
+#include "lighting_phong.glsl"
+#include "lighting_pbr.glsl"
 
 layout (location = 0) out vec3 gColor;
 
 in vec2 UV;
 
-uniform sampler2D colors;
-uniform sampler2D normals;
-uniform sampler2D RMDs;
-
-#define DEBUG
+uniform sampler2D ColorRs;
+uniform sampler2D NormalMs;
+uniform sampler2D Positions;
 
 void main() {
     vec2 MUV = UV;
@@ -20,32 +62,45 @@ void main() {
     MUV *= 2.0;
 
     if(UV.x > 0.5 && UV.y > 0.5) {
-        gColor = texture(colors, MUV).rgb;
+        gColor = texture(ColorRs, MUV).rgb;
     } else if(UV.x > 0.5 && UV.y < 0.5) {
-        gColor = texture(normals, MUV).rgb;
+        gColor = texture(NormalMs, MUV).rgb;
     } else if(UV.x < 0.5 && UV.y > 0.5) {
-        gColor = texture(RMDs, MUV).rgb;
+        gColor = texture(Positions, MUV).rgb;
     } else {
 #endif
 
-    vec4 color = texture(colors, MUV);
-    vec4 normal = texture(normals, MUV);
-    vec4 rmd = texture(RMDs, MUV);
+    vec4 ColorR     = texture(ColorRs, MUV);
+    vec4 NormalM    = texture(NormalMs, MUV);
 
-    gColor = color.xyz / 10.0;
+    vec3 Color      = gamma_decode(ColorR.rgb, 2.2);
+    vec3 Normal     = normalize(NormalM.xyz);
+    vec3 Position   = texture(Positions, MUV).xyz;
+
+    float roughness = 0.1; //ColorR.w;
+    float metallic  = NormalM.w;
+
+    if(length(Normal) > EPSILON) {
+        test_lights();
+
+        const float clearcoat = 1.0;
+        const float metallic_absorption = 0.0;
+        const float albedo = 1.0;
+        const float ior = 1.0;
+        const float anisotropy = 0.0;
+        const float anisotropic_ratio = 2.0;
+
+        gColor = calc_lighting_phong(Color, Position, Normal, view_position, albedo, metallic).rgb;
+
+        //gColor = calc_lighting_pbr(Color, Position, Normal, view_position,
+        //    roughness, clearcoat, metallic, metallic_absorption, albedo, ior, anisotropy, anisotropic_ratio).rgb;
+    }
 
 #ifdef DEBUG
     }
 
-    float dx = abs(UV.x - 0.5);
-    float dy = abs(UV.y - 0.5);
-
-    //if(dx < 0.01) {
-        gColor += vec3(1.0) * (0.0005 / dx);
-    //}
-
-    //if(dy < 0.01) {
-        gColor += vec3(1.0) * (0.0005 / dy);
-    //}
+    //Add lines between quadrants
+    gColor += vec3(1.0) * (0.0005 / abs(UV.x - 0.5));
+    gColor += vec3(1.0) * (0.0005 / abs(UV.y - 0.5));
 #endif
 }
---
+++ shaders/lighting_pbr.glsl
@@ -0,0 +1,191 @@
+#ifndef SHADER_LIGHTING_GLSL_INCLUDED
+#define SHADER_LIGHTING_GLSL_INCLUDED
+
+//Tell the Oren-Nayar algorithm to smooth out the lower 25% of the cosine
+//This is based on a similar technique used in the "mia-material" engine
+#ifndef SMOOTH_OREN_NAYAR_EDGE
+    #define SMOOTH_OREN_NAYAR_EDGE 0.25
+#endif
+
+#ifndef UNCHARTED_2_TONEMAP_REFERENCE_ARCHIVE
+    #define UNCHARTED_2_TONEMAP_REFERENCE_ARCHIVE
+#endif
+
+#include "lib/light.glsl"
+
+#include "lib/constants.glsl"       //Things like PI, EPSILON, etc
+#include "lib/utils.glsl"           //Various misc functions
+#include "lib/depth.glsl"           //Calculate position from depth
+#include "lib/smoothing.glsl"       //Smoothing/interpolation algorithms
+#include "lib/attenuation.glsl"     //Lighting attenuation equations
+#include "lib/gamma.glsl"           //Routines for gamma correction
+#include "lib/hdr.glsl"             //Tone mapping equations
+
+#include "external/PhotoshopMathFP.glsl"
+
+#include "partial/fresnel.glsl"             //Fresnel equations
+#include "partial/cook_torrance.glsl"       //Cook-Torrance functions
+#include "partial/oren_nayar.glsl"          //Oren-Nayar diffuse lighting algorithm
+#include "partial/retro_reflection.glsl"    //Diffuse retro-reflection
+#include "partial/ggx.glsl"                 //GGX (Trowbridge-Reitz) BRDF equations
+
+vec4 calc_lighting_pbr(vec3 color,
+                       vec3 position,
+                       vec3 normal,
+                       vec3 view_position,
+                       float roughness,
+                       float clearcoat,
+                       float metallic,
+                       float metallic_absorption,
+                       float albedo,
+                       float ior,
+                       float anisotropy,
+                       float anisotropic_ratio) {
+
+    vec4 linear_color = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 diffuse_color = vec4(0.0, 0.0, 0.0, 1.0) + 0.01;
+    vec4 specular_color = vec4(0.0, 0.0, 0.0, 1.0);
+
+    for(int i = 0; i < MAX_LIGHTS; i++) {
+        if(lights[i].disabled) {
+            continue;
+        }
+
+        #define light lights[i]
+
+        bool should_render = true;
+
+        float light_distance;
+        vec3  light_direction;
+
+        if(light.kind == DIRECTIONAL_LIGHT) {
+            //The negation is intentional, because the algorithms expect the opposite vector for the light direction.
+            light_direction = -light.direction;
+
+            /*Ignore light distance for directional lights. They are infinitely far away.*/
+
+        } else {
+            vec3 light_position = light.position;
+
+            light_distance  = distance(light_position, position);
+            light_direction = normalize(light_position - position);
+
+            //Since point and spot lights are NOT infinitely far away, check if we should render them at all
+            should_render   = light_distance > light.zdistance.x &&
+                              light_distance < light.zdistance.y;
+        }
+
+        float NdotL = dot(normal, light_direction);
+
+        if(should_render && NdotL > 0.0) {
+            NdotL = max(NdotL, 0.0);
+
+            //The attenuation is basically the resulting brightness of the light
+            //depending on distance from the light and the light intensity
+            float attenuation;
+
+            //Ignore attenuation for infinite lights
+            if(light.kind == DIRECTIONAL_LIGHT) {
+                attenuation = light.intensity;
+
+            } else {
+                //Get the distance attenuation which applies to all sized lights
+                attenuation = inverse_square_attenuation(light_distance, light.radius, light.intensity);
+
+                //If the light is a spotlight, apply the attenuation from the cone angles
+                if(light.kind == SPOT_LIGHT) {
+                    float theta = acos(dot(normalize(-light.direction), light_direction));
+
+                    if(theta > light.inner_cone) {
+                        if(theta > light.outer_cone) {
+                            attenuation = 0.0;
+
+                        } else {
+                            attenuation *= smooth_cos_clamped2(light.inner_cone, light.outer_cone, theta);
+                        }
+                    }
+
+                    float multiplier = (TAU / (light.inner_cone + light.outer_cone)) - 1.0;
+
+                    attenuation += attenuation * light.reflector_efficiency * multiplier;
+                }
+            }
+
+            //Don't bother continuing if it won't be illuminated anyway
+            if(attenuation >= ATTENUATION_THRESHOLD) {
+                //Results
+                vec3  specular = vec3(0.0);
+                float diffuse = 0.0;
+
+                //Vectors
+                vec3 view_direction = normalize(view_position - position);
+                vec3 half_direction = normalize(light_direction + view_direction);
+
+                //Dot products, saturated
+                float NdotH = dot(normal, half_direction);
+                float NdotV = dot(normal, view_direction);
+                float VdotH = dot(view_direction, half_direction);
+                float VdotL = dot(view_direction, light_direction);
+
+                //roughness squared
+                float sigma2 = roughness * roughness;
+
+                //Compute fresnel ratio term
+                float F0 = fresnel0(ior);
+
+                /// Oren-Nayar diffuse part plus retro-reflection
+
+                diffuse = oren_nayar(sigma2, albedo, metallic, NdotL, NdotV, F0, view_direction, light_direction, normal);
+
+                diffuse = apply_retro_reflection(diffuse, roughness, albedo, metallic, NdotV, NdotL, VdotH);
+
+                /// Modified Cook-Torrance specular part
+
+                //Terms
+                float D, G;
+                vec3  F;
+
+                //Geometric attenuation part using weighted anisotropic/isotropic attenuations
+                float NdotH2 = NdotH * NdotH;
+
+                D = ggx_distribution(sigma2, NdotH2);
+
+                //Just use the purely dialectric fresnel if no conductivity is specified
+                F = vec3(fresnel_schlick(F0, VdotH));
+
+                //Smith form of the goemetric attenuations
+                G = ggx_geo_attenuation(sigma2, NdotL) *
+                    ggx_geo_attenuation(sigma2, NdotV);
+
+                //Combine all the terms using the Cook-Torrance form
+                specular =     (D * F * G) /
+                          (NdotV * NdotL * 4.0);
+
+                diffuse_color.rgb += (diffuse * (1.0 - specular)) * light.color.rgb * attenuation;
+                specular_color.rgb += specular * light.color.rgb * attenuation;
+            }
+        }
+    }
+
+    vec3 blended_specular_color = mix(BlendMultiply(Desaturate(color.rgb, 1.0 - clearcoat), specular_color.rgb),
+                                      mix(specular_color.rgb, BlendHardLight(color.rgb, specular_color.rgb), roughness),
+                                      clearcoat);
+
+    //Merge diffuse and specular components with object tint
+    linear_color.rgb += BlendMultiply(color.rgb, diffuse_color.rgb) + blended_specular_color;
+    //linear_color.rgb += specular_color.rgb;
+
+    //Map HDR into linear space
+    linear_color.rgb = ACESFilm_tonemap_exposure(linear_color.rgb, 2.0);
+
+    //Convert linear color into monitor gamma space
+    vec4 final_color = gamma_encode(linear_color, 2.2);
+
+    //Premultiply color by alpha before blending
+    final_color.rgb *= final_color.a;
+
+    return final_color;
+}
+
+
+#endif //SHADER_LIGHTING_GLSL_INCLUDED
