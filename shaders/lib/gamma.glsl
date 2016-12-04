19fc70d9bb8bcffa00bdb30f9b642658e03e774e
Aaron Trent <novacrazy@gmail.com>
2016-12-04 02:20:30 -0600

    Add vec3 gamma functions

--- shaders/lib/gamma.glsl
+++ shaders/lib/gamma.glsl
@@ -9,4 +9,13 @@ vec4 gamma_decode(vec4 color, float gamma) {
     return pow(color, vec4(gamma));
 }
 
+vec3 gamma_encode(vec3 color, float gamma) {
+    return pow(color, vec3(1.0 / gamma));
+}
+
+vec3 gamma_decode(vec3 color, float gamma) {
+    return pow(color, vec3(gamma));
+}
+
+
 #endif //SHADER_LIB_GAMMA_GLSL_INCLUDED
