18e89159b58e1c9ef4c82d1f8a31ae4b42314e62
Aaron Trent <novacrazy@gmail.com>
2016-12-04 02:24:08 -0600

    Ignore other transforms if isometry is specified. For now..

--- src/systems/transform.rs
+++ src/systems/transform.rs
@@ -40,31 +40,31 @@ impl System {
             let mut translation_matrix = Matrix4::new_identity(4);
             let mut isometry_matrix = Matrix4::new_identity(4);
 
+            if let Some(isometry) = isometries.get(entity) {
+                isometry_matrix = isometry.0.to_homogeneous();
+            } else {
+                if let Some(quat_rotation) = quat_rotations.get(entity) {
+                    //Normalize the quaternion to the UnitQuaternion,
+                    // then convert it to rotation matrix,
+                    // then convert that to a homogeneous matrix and apply it
+                    quat_rotation_matrix = to_rotation_matrix(&quat_rotation.unit()).to_homogeneous();
+                }
+
+                if let Some(rotation) = rotations.get(entity) {
+                    rotation_matrix *= rotation.0.to_homogeneous();
+                }
+
+                if let Some(position) = positions.get(entity) {
+                    translation_matrix.m14 = position.0.x;
+                    translation_matrix.m24 = position.0.y;
+                    translation_matrix.m34 = position.0.z;
+                }
+            }
+
             if let Some(scale) = scales.get(entity) {
                 scale_matrix.m11 = scale.0.x;
                 scale_matrix.m22 = scale.0.y;
                 scale_matrix.m33 = scale.0.z;
-            }
-
-            if let Some(isometry) = isometries.get(entity) {
-                isometry_matrix = isometry.0.to_homogeneous();
-            }
-
-            if let Some(quat_rotation) = quat_rotations.get(entity) {
-                //Normalize the quaternion to the UnitQuaternion,
-                // then convert it to rotation matrix,
-                // then convert that to a homogeneous matrix and apply it
-                quat_rotation_matrix = to_rotation_matrix(&quat_rotation.unit()).to_homogeneous();
-            }
-
-            if let Some(rotation) = rotations.get(entity) {
-                rotation_matrix *= rotation.0.to_homogeneous();
-            }
-
-            if let Some(position) = positions.get(entity) {
-                translation_matrix.m14 = position.0.x;
-                translation_matrix.m24 = position.0.y;
-                translation_matrix.m34 = position.0.z;
             }
 
             transform.matrix = isometry_matrix * translation_matrix * rotation_matrix * quat_rotation_matrix * scale_matrix;
