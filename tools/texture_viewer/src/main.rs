2e8e1b5bd5c362ea681e7b96a3752f2b2d017ef3
Aaron Trent <novacrazy@gmail.com>
2016-12-09 07:30:56 -0600

    Add texture viewer tool WIP
    Former-commit-id: 39cfa3dfa8b8e7f169fed7096ac08493f274cd7a

--- tools/texture_viewer/src/main.rs
+++ tools/texture_viewer/src/main.rs
@@ -1,6 +1,5 @@
 extern crate glfw;
 extern crate nice_glfw;
-extern crate image;
 extern crate clap;
 
 #[macro_use]
@@ -26,12 +25,12 @@ fn init() {
 
     let (mut window, _) = nice_glfw::WindowBuilder::new(&mut glfw)
         .try_modern_context_hints()
-        .size(1, 1)
+        .size(600, 800)
         .common_hints(&[
-            WindowHint::Visible(false),
+            WindowHint::Visible(true),
             WindowHint::OpenGlDebugContext(true)
         ])
-        .title("texture_compressor")
+        .title("Combustion Texture Viewer")
         .create()
         .expect_logged("Couldn't create window");
 
@@ -44,40 +43,12 @@ fn init() {
     backend::gl::enable_debug(backend::gl::default_debug_callback, true).unwrap();
 }
 
-fn compress_texture<P: AsRef<Path>>(path: P, dir: &Path) -> GLResult<()> {
-    let stem = path.as_ref().file_stem().unwrap();
-
-
-
-    Ok(())
-}
-
 fn main() {
-    let matches: clap::ArgMatches = App::new("texture_compressor")
+    let matches: clap::ArgMatches = App::new("texture_viewer")
         .version("0.1.0")
         .author("Aaron Trent <novacrazy@gmail.com>")
-        .about("Converts image files into compressed textures")
-        .arg(Arg::with_name("files").multiple(true).required(true).help("Images to compress").validator(|ref path| -> Result<(), String> {
-            if Path::new(path).extension().is_some() {
-                Ok(())
-            } else {
-                Err("the images must have file extensions".to_string())
-            }
-        }))
-        .arg(Arg::with_name("out_dir").short("o").help("Output directory").takes_value(true))
+        .about("Allows Combustion textures to be viewed easily")
         .get_matches();
 
-    if let Some(files) = matches.values_of("files") {
-        init();
-
-        let out_dir = matches.value_of("out_dir").map(|d| Path::new(d));
-
-        for file in files {
-            let dir = if let Some(out_dir) = out_dir { out_dir } else {
-                Path::new(file).parent().unwrap_or(Path::new("."))
-            };
-
-            compress_texture(file, dir).expect_logged("Could not process file");
-        }
-    }
+    init();
 }
