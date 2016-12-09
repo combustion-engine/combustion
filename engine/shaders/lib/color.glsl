48cfdf29074258ac89098e64bfbea7fa7a8a519d
Aaron Trent <novacrazy@gmail.com>
2016-12-08 19:42:28 -0600

    Rename basic contrast functions
    Former-commit-id: dfd4fa139b80e8292e1b790f41ed083761268818

--- engine/shaders/lib/color.glsl
+++ engine/shaders/lib/color.glsl
@@ -1,19 +1,19 @@
 #ifndef SHADER_LIB_COLOR_GLSL_INCLUDED
 #define SHADER_LIB_COLOR_GLSL_INCLUDED
 
-float contrast(float value, float c) {
+float basic_contrast(float value, float c) {
     return ((value - 0.5) * max(c, 0.0)) + 0.5;
 }
 
-vec2 contrast(vec2 value, float c) {
+vec2 basic_contrast(vec2 value, float c) {
     return ((value - 0.5) * max(c, 0.0)) + 0.5;
 }
 
-vec3 contrast(vec3 value, float c) {
+vec3 basic_contrast(vec3 value, float c) {
     return ((value - 0.5) * max(c, 0.0)) + 0.5;
 }
 
-vec4 contrast(vec4 value, float c) {
+vec4 basic_contrast(vec4 value, float c) {
     return ((value - 0.5) * max(c, 0.0)) + 0.5;
 }
 
