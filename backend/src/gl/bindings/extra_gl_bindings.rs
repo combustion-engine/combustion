2791e2b7e5934323778dda0ecd950513babae17e
Aaron Trent <novacrazy@gmail.com>
2016-12-10 08:13:38 -0600

    Add a bunch of OpenGL texture compression extension constants
    Former-commit-id: af8f0c14b31b368fc2a865c06f6f5bb97e5a0c3d

--- backend/src/gl/bindings/extra_gl_bindings.rs
+++ backend/src/gl/bindings/extra_gl_bindings.rs
@@ -1,12 +1,67 @@
-#![allow(bad_style)]
+//! OpenGL extensions and such that aren't covered by the standard bindings
+//!
+//! References:
+//!
+//! * https://www.opengl.org/registry/specs/EXT/texture_filter_anisotropic.txt
+//! * https://www.opengl.org/registry/specs/ARB/texture_compression_bptc.txt
+//! * http://www.opengl.org/registry/specs/EXT/texture_compression_s3tc.txt
+//! * https://www.opengl.org/registry/specs/KHR/texture_compression_astc_hdr.txt
+//!
+#![allow(bad_style, clippy)]
 
 use super::gl_bindings::*;
 
 use std::mem;
 use std::os::raw;
 
-pub const MAX_TEXTURE_MAX_ANISOTROPY_EXT: types::GLenum = 0x84FF;
+pub const EXT_texture_filter_anisotropic: &'static str = "GL_EXT_texture_filter_anisotropic";
+pub const EXT_texture_compression_s3tc: &'static str = "GL_EXT_texture_compression_s3tc";
+pub const ARB_texture_compression_bptc: &'static str = "GL_ARB_texture_compression_bptc";
+pub const KHR_texture_compression_astc_hdr: &'static str = "GL_KHR_texture_compression_astc_hdr";
+pub const KHR_texture_compression_astc_ldr: &'static str = "GL_KHR_texture_compression_astc_ldr";
+
 pub const TEXTURE_MAX_ANISOTROPY_EXT: types::GLenum = 0x84FE;
+pub const MAX_TEXTURE_MAX_ANISOTROPY_EXT: types::GLenum = 0x84FF;
+
+pub const COMPRESSED_RGB_S3TC_DXT1_EXT: types::GLenum = 0x83F0;
+pub const COMPRESSED_RGBA_S3TC_DXT1_EXT: types::GLenum = 0x83F1;
+pub const COMPRESSED_RGBA_S3TC_DXT3_EXT: types::GLenum = 0x83F2;
+pub const COMPRESSED_RGBA_S3TC_DXT5_EXT: types::GLenum = 0x83F3;
+
+pub const COMPRESSED_RGBA_BPTC_UNORM_ARB: types::GLenum = 0x8E8C;
+pub const COMPRESSED_SRGB_ALPHA_BPTC_UNORM_ARB: types::GLenum = 0x8E8D;
+pub const COMPRESSED_RGB_BPTC_SIGNED_FLOAT_ARB: types::GLenum = 0x8E8E;
+pub const COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT_ARB: types::GLenum = 0x8E8F;
+
+pub const COMPRESSED_RGBA_ASTC_4x4_KHR: types::GLenum = 0x93B0;
+pub const COMPRESSED_RGBA_ASTC_5x4_KHR: types::GLenum = 0x93B1;
+pub const COMPRESSED_RGBA_ASTC_5x5_KHR: types::GLenum = 0x93B2;
+pub const COMPRESSED_RGBA_ASTC_6x5_KHR: types::GLenum = 0x93B3;
+pub const COMPRESSED_RGBA_ASTC_6x6_KHR: types::GLenum = 0x93B4;
+pub const COMPRESSED_RGBA_ASTC_8x5_KHR: types::GLenum = 0x93B5;
+pub const COMPRESSED_RGBA_ASTC_8x6_KHR: types::GLenum = 0x93B6;
+pub const COMPRESSED_RGBA_ASTC_8x8_KHR: types::GLenum = 0x93B7;
+pub const COMPRESSED_RGBA_ASTC_10x5_KHR: types::GLenum = 0x93B8;
+pub const COMPRESSED_RGBA_ASTC_10x6_KHR: types::GLenum = 0x93B9;
+pub const COMPRESSED_RGBA_ASTC_10x8_KHR: types::GLenum = 0x93BA;
+pub const COMPRESSED_RGBA_ASTC_10x10_KHR: types::GLenum = 0x93BB;
+pub const COMPRESSED_RGBA_ASTC_12x10_KHR: types::GLenum = 0x93BC;
+pub const COMPRESSED_RGBA_ASTC_12x12_KHR: types::GLenum = 0x93BD;
+
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_4x4_KHR: types::GLenum = 0x93D0;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_5x4_KHR: types::GLenum = 0x93D1;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_5x5_KHR: types::GLenum = 0x93D2;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_6x5_KHR: types::GLenum = 0x93D3;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_6x6_KHR: types::GLenum = 0x93D4;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_8x5_KHR: types::GLenum = 0x93D5;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_8x6_KHR: types::GLenum = 0x93D6;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_8x8_KHR: types::GLenum = 0x93D7;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_10x5_KHR: types::GLenum = 0x93D8;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_10x6_KHR: types::GLenum = 0x93D9;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_10x8_KHR: types::GLenum = 0x93DA;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_10x10_KHR: types::GLenum = 0x93DB;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_12x10_KHR: types::GLenum = 0x93DC;
+pub const COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR: types::GLenum = 0x93DD;
 
 mod storage {
     use super::super::gl_bindings::{FnPtr, missing_fn_panic};
