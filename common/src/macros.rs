5e90e56931ea8da19e8d7077cdc6a3adbecbd322
Aaron Trent <novacrazy@gmail.com>
2016-12-11 07:21:21 -0600

    Remove old macros
    Former-commit-id: 1ff4a494ad18a4117f253caeb5d551d1f4cd3be7

--- common/src/macros.rs
+++ common/src/macros.rs
@@ -1,4 +1,3 @@
-#[macro_export]
 macro_rules! err {
     ($($arg:tt)*) => {{
         use std::io::Write;
@@ -6,56 +5,15 @@ macro_rules! err {
     }};
 }
 
-#[macro_export]
 macro_rules! errln {
     ($fmt:expr) => (err!(concat!($fmt, "\n")));
     ($fmt:expr, $($arg:tt)*) => (err!(concat!($fmt, "\n"), $($arg)*));
 }
 
 #[macro_export]
-#[cfg(debug)]
-macro_rules! debug_print {
-    ($($arg:tt)*) => (print!($($arg)*))
-}
-
-#[macro_export]
-#[cfg(debug)]
-macro_rules! debug_println {
-    ($($arg:tt)*) => (println!($($arg)*))
-}
-
-#[macro_export]
-#[cfg(debug)]
-macro_rules! debug_err {
-    ($($arg:tt)*) => (err!($($arg)*))
-}
-
-#[macro_export]
-#[cfg(debug)]
-macro_rules! debug_errln {
-    ($($arg:tt)*) => (errln!($($arg)*))
-}
-
-#[macro_export]
-#[cfg(not(debug))]
-macro_rules! debug_print {
-    ($($arg:tt)*) => {}
-}
-
-#[macro_export]
-#[cfg(not(debug))]
-macro_rules! debug_println {
-    ($($arg:tt)*) => ()
-}
-
-#[macro_export]
-#[cfg(not(debug))]
-macro_rules! debug_err {
-    ($($arg:tt)*) => {}
-}
-
-#[macro_export]
-#[cfg(not(debug))]
-macro_rules! debug_errln {
-    ($($arg:tt)*) => ()
+macro_rules! error_panic {
+    ($($arg:tt)*) => {{
+        error!($($arg)*);
+        panic!($($arg)*);
+    }}
 }
