93e02008f623232b944d665340310947c50852e1
Aaron Trent <novacrazy@gmail.com>
2016-12-14 04:47:53 -0600

    Put windows and DX/Vulkan stuff behind feature flags
    Former-commit-id: e8c121013ec3fa8cf80dd38766ee20a2c6364d9d

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -7,11 +7,14 @@
 #![allow(unused_imports, unknown_lints)]
 #![feature(proc_macro)]
 
+extern crate libc;
+#[cfg(all(feature = "dx11", target_os = "windows"))]
+extern crate winapi;
+
 #[macro_use]
 extern crate enum_primitive;
 #[macro_use]
 extern crate lazy_static;
-extern crate libc;
 extern crate nalgebra;
 extern crate num_traits;
 extern crate image;
@@ -25,9 +28,15 @@ extern crate serde_derive;
 #[macro_use]
 extern crate combustion_common;
 
-pub mod gl;
-pub mod vulkan;
-pub mod dx11;
 pub mod generic;
+
+#[cfg(feature = "gl")]
+pub mod gl;
+
+#[cfg(feature = "vulkan")]
+pub mod vulkan;
+
+#[cfg(all(feature = "dx11", target_os = "windows"))]
+pub mod dx11;
 
 pub mod command;
