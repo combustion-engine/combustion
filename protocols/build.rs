2fb81f85cbb01dd64d7627caec09e1c4da922728
Aaron Trent <novacrazy@gmail.com>
2016-12-17 02:38:52 -0600

    Add support for building Cap'N Proto C++ libraries
    Former-commit-id: ba6baa61f9127327ea91019ccb54a7d2443c68db

--- protocols/build.rs
+++ protocols/build.rs
@@ -1,40 +1,93 @@
 #[macro_use]
 extern crate combustion_common as common;
 extern crate capnpc;
+extern crate cmake;
+extern crate gcc;
 
+use std::process::Command;
 use std::env;
 use std::fs::*;
 use std::path::Path;
 
-/// Recursively visit directories
-fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) {
-    if dir.is_dir() {
-        for entry in read_dir(dir).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-
-            if path.is_dir() { visit_dirs(&path, cb); } else { cb(&entry); }
-        }
-    }
-}
+use common::error::*;
+use common::utils;
 
 /// Visit directories, find .capnp files, compile them, then replace absolute module references with `super` in the output code.
-fn compile_capnprotos(_out_dir: String) {
-    visit_dirs(Path::new("protocols"), &|entry: &DirEntry| {
+fn compile_capnprotos() {
+    info!("Compiling Cap'N Proto protocols");
+
+    utils::fs::visit_dirs(Path::new("protocols"), &|entry: &DirEntry| {
         if let Some(ext) = entry.path().as_path().extension() {
             if ext == "capnp" {
-                info!("Attempting to compile: {:?}", entry.path());
+                info!("Attempting to generate: {:?} as Rust", entry.path());
 
-                if let Err(err) = capnpc::CompilerCommand::new().file(entry.path()).include("protocols").run() {
-                    error!("Failed to compile protocol: {}", err);
-                    panic!("Failed to compile protocol: {}", err);
-                } else {
-                    info!("Success!");
+                capnpc::CompilerCommand::new().file(entry.path()).include("protocols").run().expect_logged("Failed to compile protocol");
+
+                #[cfg(feature = "cpp")]
+                {
+                    info!("Attempting to generate: {:?} as C++", entry.path());
+                    let output = Command::new("capnp.exe").arg("compile").arg("-oc++").arg("-Iprotocols").arg(entry.path()).output()
+                                                          .expect_logged("Failed to compile protocol");
+
+                    if !output.status.success() {
+                        error!("Output: {:?}", output);
+                        panic!("Output: {:?}", output);
+                    }
                 }
-            }
 
+                info!("Success!");
+            }
         }
     });
+
+    #[cfg(feature = "cpp")]
+    {
+        info!("Moving generated C++ code to cpp directory");
+
+        Command::new("mv")
+            .arg("protocols/*.capnp.h")
+            .arg("protocols/*.capnp.c++")
+            .arg("protocols/*.capnp.cpp")
+            .arg("cpp")
+            .output()
+            .expect_logged("Could not move files");
+
+        info!("Success!");
+    }
+
+    info!("Finished all Cap'N Proto protocols");
+}
+
+#[cfg(feature = "cpp")]
+fn build_cpp(out_dir: String) {
+    let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    info!("Compiling Cap'N Proto C++ static library");
+
+    let mut capnp_config = cmake::Config::new("cpp/capnproto/c++");
+
+    capnp_config.define("CAPNP_LITE", "1");
+    capnp_config.define("EXTERNAL_CAPNP", "1");
+    capnp_config.define("BUILD_TOOLS", "OFF");
+    capnp_config.define("BUILD_TESTING", "OFF");
+
+    capnp_config.define("CAPNP_EXECUTABLE", format!("{}/capnp.exe", src_dir));
+    capnp_config.define("CAPNP_CXX_EXECUTABLE", format!("{}/capnpc-c++.exe", src_dir));
+    capnp_config.define("CAPNP_INCLUDE_DIRECTORY", format!("{}/cpp/capnproto/c++/src/capnp/", src_dir));
+
+    let capnp_dst = capnp_config.build();
+
+    println!("cargo:rustc-link-search=native={}", capnp_dst.join("lib").display());
+    println!("cargo:rustc-link-lib=static=capnp");
+    println!("cargo:rustc-link-lib=static=kj");
+
+    Command::new("cp").arg("-R")
+                      .arg(format!("{}/include/*", out_dir))
+                      .arg("cpp/include/")
+                      .output()
+                      .expect_logged("Failed to copy include files");
+
+    info!("Success!");
 }
 
 fn main() {
@@ -42,7 +95,8 @@ fn main() {
 
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    info!("Compiling Cap'N Proto protocols");
-    compile_capnprotos(out_dir.clone());
-    info!("Finished all Cap'N Proto protocols");
+    compile_capnprotos();
+
+    #[cfg(feature = "cpp")]
+    build_cpp(out_dir.clone());
 }
