4415edc6836fb3a20fe95cd60fee0c6b19f97731
Aaron Trent <novacrazy@gmail.com>
2016-12-19 00:33:04 -0600

    Switch to serde_yaml for tests, and presumably for future stuff, too
    Former-commit-id: 2d8777ffa05087d562d53f06254546db37598d5c

--- protocols/tests/material_sample.rs
+++ protocols/tests/material_sample.rs
@@ -1,11 +1,25 @@
 extern crate combustion_protocols as protocols;
-extern crate serde_hjson;
+extern crate serde_yaml;
 
+use std::fs::File;
+
+use protocols::material::MaterialMap;
 use protocols::material::sample::sample as sample_material;
 
 #[test]
-pub fn json_test() {
-    use serde_hjson::to_string;
+pub fn yaml_test() {
+    use serde_yaml::to_string;
 
     println!("MaterialMap {}", to_string(&sample_material()).unwrap());
+}
+
+#[test]
+pub fn parse_test() {
+    use serde_yaml::from_reader;
+
+    let src = File::open("tests/material.yaml").unwrap();
+
+    let parsed: MaterialMap = from_reader(src).unwrap();
+
+    println!("{:?}", parsed);
 }
