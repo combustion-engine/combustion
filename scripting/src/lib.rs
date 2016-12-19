3768242c2e0dfb571101e95a2812563b274474ab
Aaron Trent <novacrazy@gmail.com>
2016-12-19 16:17:59 -0600

    Add initial scripting crate
    Former-commit-id: fb89648b30bb873a745fb26dc59d4a7342fa7fdb

---
+++ scripting/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate capnp;
+extern crate capnpc;
+
+#[macro_use]
+extern crate combustion_common as common;
+#[macro_use]
+extern crate combustion_backend as backend;
+extern crate combustion_protocols;
+
+extern crate squirrel;
