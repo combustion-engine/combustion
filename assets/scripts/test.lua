97925b41a7be58f923effcc89fe4e50006a4515f
Aaron Trent <novacrazy@gmail.com>
2016-12-19 16:19:53 -0600

    Add test lua script asset
    Former-commit-id: 27e34bb735d668151c43f218394e957a944f8867

---
+++ assets/scripts/test.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Aaron
+-- Date: 12/19/2016
+-- Time: 4:08 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+print "Hello, World!"
