62957526fb3770c55065eac15c4ee6e6b59d5a9a
Aaron Trent <novacrazy@gmail.com>
2016-12-21 12:29:41 -0600

    Rewrite human readable utils to be faster and more flexible, with tests
    Former-commit-id: fcf53e2453e9b29b43a2849e0133cdbe43014ab8

--- common/src/utils/human_readable.rs
+++ common/src/utils/human_readable.rs
@@ -1,15 +1,63 @@
 use std::cmp;
 
-pub fn convert(num: f64) -> String {
-    let negative = if num.is_sign_positive() { "" } else { "-" };
-    let num = num.abs();
-    let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    if num < 1_f64 {
-        return format!("{}{} {}", negative, num, "B");
+pub const UNITS: [&'static str; 9] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+pub const SI_UNITS: [&'static str; 9] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+
+pub fn humanize(num: f64) -> String {
+    const DELIMITER: f64 = 1000.0;
+    const DELIMITER_LOG: f64 = 3.0; //log10
+
+    if num < 1_f64 && num > -1_f64 {
+        format!("{} {}", num, "B")
+    } else {
+        let exponent = cmp::min((num.abs().log10() / DELIMITER_LOG).floor() as i32,
+                                (UNITS.len() - 1) as i32);
+
+        format!("{:.2} {}", num / DELIMITER.powi(exponent), UNITS[exponent as usize])
     }
-    let delimiter = 1000_f64;
-    let exponent = cmp::min((num.ln() / delimiter.ln()).floor() as i32, (units.len() - 1) as i32);
-    let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
-    let unit = units[exponent as usize];
-    format!("{}{} {}", negative, pretty_bytes, unit)
+}
+
+pub fn humanize_si(num: f64) -> String {
+    const DELIMITER: f64 = 1024.0;
+    const DELIMITER_LOG: f64 = 10.0; //log2
+
+    if num <= 1_f64 && num >= -1_f64 {
+        format!("{} {}", num, "B")
+    } else {
+        let exponent = cmp::min((num.abs().log2() / DELIMITER_LOG).floor() as i32,
+                                (SI_UNITS.len() - 1) as i32);
+
+        format!("{:.2} {}", num / DELIMITER.powi(exponent), SI_UNITS[exponent as usize])
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_humanize() {
+        assert_eq!(humanize(0.1), "0.1 B");
+        assert_eq!(humanize(10.0), "10.00 B");
+        assert_eq!(humanize(4_321.00), "4.32 kB");
+        assert_eq!(humanize(4_321_000.00), "4.32 MB");
+        assert_eq!(humanize(4_321_000_000.00), "4.32 GB");
+        assert_eq!(humanize(4_321_000_000_000.00), "4.32 TB");
+
+        assert_eq!(humanize(-4_321.00), "-4.32 kB");
+        assert_eq!(humanize(-0.1), "-0.1 B");
+    }
+
+    #[test]
+    fn test_humanize_si() {
+        assert_eq!(humanize_si(0.1), "0.1 B");
+        assert_eq!(humanize_si(10.0), "10.00 B");
+        assert_eq!(humanize_si(4_321.00), "4.22 KiB");
+        assert_eq!(humanize_si(4_321_000.00), "4.12 MiB");
+        assert_eq!(humanize_si(4_321_000_000.00), "4.02 GiB");
+        assert_eq!(humanize_si(4_321_000_000_000.00), "3.93 TiB");
+
+        assert_eq!(humanize_si(-4_321.00), "-4.22 KiB");
+        assert_eq!(humanize_si(-0.1), "-0.1 B");
+    }
 }
