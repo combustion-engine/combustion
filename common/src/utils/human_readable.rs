7a05030d3207fa68b0774c56935566dbe7af58e0
Aaron Trent <novacrazy@gmail.com>
2016-12-21 12:42:24 -0600

    Rename human readable stuff to the correct names
    Former-commit-id: bdd2fe9047a7bef74163ccd95000877396e9146e

--- common/src/utils/human_readable.rs
+++ common/src/utils/human_readable.rs
@@ -1,9 +1,9 @@
 use std::cmp;
 
-pub const UNITS: [&'static str; 9] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-pub const SI_UNITS: [&'static str; 9] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+pub const SI_UNITS: [&'static str; 9] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+pub const IEC_UNITS: [&'static str; 9] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
 
-pub fn humanize(num: f64) -> String {
+pub fn humanize_si(num: f64) -> String {
     const DELIMITER: f64 = 1000.0;
     const DELIMITER_LOG: f64 = 3.0; //log10
 
@@ -11,13 +11,13 @@ pub fn humanize(num: f64) -> String {
         format!("{} {}", num, "B")
     } else {
         let exponent = cmp::min((num.abs().log10() / DELIMITER_LOG).floor() as i32,
-                                (UNITS.len() - 1) as i32);
+                                (SI_UNITS.len() - 1) as i32);
 
-        format!("{:.2} {}", num / DELIMITER.powi(exponent), UNITS[exponent as usize])
+        format!("{:.2} {}", num / DELIMITER.powi(exponent), SI_UNITS[exponent as usize])
     }
 }
 
-pub fn humanize_si(num: f64) -> String {
+pub fn humanize_iec(num: f64) -> String {
     const DELIMITER: f64 = 1024.0;
     const DELIMITER_LOG: f64 = 10.0; //log2
 
@@ -25,9 +25,9 @@ pub fn humanize_si(num: f64) -> String {
         format!("{} {}", num, "B")
     } else {
         let exponent = cmp::min((num.abs().log2() / DELIMITER_LOG).floor() as i32,
-                                (SI_UNITS.len() - 1) as i32);
+                                (IEC_UNITS.len() - 1) as i32);
 
-        format!("{:.2} {}", num / DELIMITER.powi(exponent), SI_UNITS[exponent as usize])
+        format!("{:.2} {}", num / DELIMITER.powi(exponent), IEC_UNITS[exponent as usize])
     }
 }
 
@@ -36,28 +36,28 @@ pub mod test {
     use super::*;
 
     #[test]
-    fn test_humanize() {
-        assert_eq!(humanize(0.1), "0.1 B");
-        assert_eq!(humanize(10.0), "10.00 B");
-        assert_eq!(humanize(4_321.00), "4.32 kB");
-        assert_eq!(humanize(4_321_000.00), "4.32 MB");
-        assert_eq!(humanize(4_321_000_000.00), "4.32 GB");
-        assert_eq!(humanize(4_321_000_000_000.00), "4.32 TB");
-
-        assert_eq!(humanize(-4_321.00), "-4.32 kB");
-        assert_eq!(humanize(-0.1), "-0.1 B");
-    }
-
-    #[test]
     fn test_humanize_si() {
         assert_eq!(humanize_si(0.1), "0.1 B");
         assert_eq!(humanize_si(10.0), "10.00 B");
-        assert_eq!(humanize_si(4_321.00), "4.22 KiB");
-        assert_eq!(humanize_si(4_321_000.00), "4.12 MiB");
-        assert_eq!(humanize_si(4_321_000_000.00), "4.02 GiB");
-        assert_eq!(humanize_si(4_321_000_000_000.00), "3.93 TiB");
+        assert_eq!(humanize_si(4_321.00), "4.32 kB");
+        assert_eq!(humanize_si(4_321_000.00), "4.32 MB");
+        assert_eq!(humanize_si(4_321_000_000.00), "4.32 GB");
+        assert_eq!(humanize_si(4_321_000_000_000.00), "4.32 TB");
 
-        assert_eq!(humanize_si(-4_321.00), "-4.22 KiB");
+        assert_eq!(humanize_si(-4_321.00), "-4.32 kB");
         assert_eq!(humanize_si(-0.1), "-0.1 B");
+    }
+
+    #[test]
+    fn test_humanize_iec() {
+        assert_eq!(humanize_iec(0.1), "0.1 B");
+        assert_eq!(humanize_iec(10.0), "10.00 B");
+        assert_eq!(humanize_iec(4_321.00), "4.22 KiB");
+        assert_eq!(humanize_iec(4_321_000.00), "4.12 MiB");
+        assert_eq!(humanize_iec(4_321_000_000.00), "4.02 GiB");
+        assert_eq!(humanize_iec(4_321_000_000_000.00), "3.93 TiB");
+
+        assert_eq!(humanize_iec(-4_321.00), "-4.22 KiB");
+        assert_eq!(humanize_iec(-0.1), "-0.1 B");
     }
 }
