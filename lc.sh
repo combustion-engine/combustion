2c154318cc5a794f7809cae027fe61242024b45e
Aaron Trent <novacrazy@gmail.com>
2016-12-23 00:21:06 -0600

    Update lc script
    Former-commit-id: d9234a3cc22da8976ffb5c55904c74c9275745f0

--- lc.sh
+++ lc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find . | grep -E "\.(glsl|rs|capnp|toml)$" | grep -vE "(target|/gl_bindings|external|deps/(specs|nalgebra|lz4-rs|capnpc-rust))" | xargs wc -l
+find . | grep -E "\.(glsl|rs|capnp|toml)$" | grep -vE "(target|/gl_bindings|external|deps/(specs|nalgebra|lz4-rs|capnpc-rust)|cpp/capnproto|glTF)" | xargs wc -l
