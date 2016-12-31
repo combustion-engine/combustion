a73ff4d61a8e3a32ca6425a5fc9af88d21d1cea8
Aaron Trent <novacrazy@gmail.com>
2016-12-31 06:33:43 -0600

    Add more flexible node system with dynamic typed property maps.
    Former-commit-id: ebbca2ed453f4b3b5630fbdc9cfe680a551daab9

--- combustion_scene/src/graph.rs
+++ combustion_scene/src/graph.rs
@@ -5,11 +5,17 @@ use specs::{Entity, World};
 use fnv::FnvHashMap;
 
 use error::*;
+use node::SceneNode;
 
+/// Numeric index type
 pub type Ix = usize;
 
+//TODO
+pub struct SceneEdge {}
+
+
 pub struct SceneGraph {
-    graph: StableDiGraph<Entity, (), Ix>,
+    graph: StableDiGraph<SceneNode, SceneEdge, Ix>,
     root: NodeIndex<Ix>,
     entity_table: FnvHashMap<Entity, NodeIndex<Ix>>,
 }
@@ -18,7 +24,7 @@ impl SceneGraph {
     pub fn new(world: &World) -> SceneGraph {
         let mut graph = StableDiGraph::default();
 
-        let root = graph.add_node(world.create_later());
+        let root = graph.add_node(SceneNode::new(world.create_later()));
 
         SceneGraph { graph: graph, root: root, entity_table: FnvHashMap::default() }
     }
@@ -26,38 +32,47 @@ impl SceneGraph {
     #[inline(always)]
     pub fn root(&self) -> NodeIndex<Ix> { self.root }
 
+    /// Adds a new scene node to the graph with the given parent, and returns the new node's index
     pub fn add_child(&mut self, parent: NodeIndex<Ix>, entity: Entity) -> NodeIndex<Ix> {
-        let child_node = self.graph.add_node(entity);
+        let child_node = self.graph.add_node(SceneNode::new(entity));
 
         self.entity_table.insert(entity, child_node);
 
-        self.graph.add_edge(parent, child_node, ());
+        self.graph.add_edge(parent, child_node, SceneEdge {});
 
         child_node
     }
 
+    /// Adds a new scene node to the graph with root as its parent, and returns the new node's index.
     #[inline]
-    pub fn add_root(&mut self, entity: Entity) -> NodeIndex<Ix> {
+    pub fn add_node(&mut self, entity: Entity) -> NodeIndex<Ix> {
         let root = self.root;
 
         self.add_child(root, entity)
     }
 
+    /// Find the node index for a specific entity using a lookup table.
+    ///
+    /// This operation is `O(1)`
     #[inline]
     pub fn lookup_index(&self, entity: Entity) -> Option<&NodeIndex<Ix>> {
         self.entity_table.get(&entity)
     }
 
+    /// Find the node value from the given index.
+    ///
+    /// This operation is `O(1)`
     #[inline]
-    pub fn lookup_entity(&self, index: NodeIndex<Ix>) -> Option<&Entity> {
+    pub fn lookup_entity(&self, index: NodeIndex<Ix>) -> Option<&SceneNode> {
         self.graph.node_weight(index)
     }
 
+    /// This operation is `O(e' + e')` for the two edge lookups.
     pub fn reparent(&mut self, child: NodeIndex<Ix>, old_parent: NodeIndex<Ix>, new_parent: NodeIndex<Ix>) -> SceneResult<()> {
         if let Some(edge) = self.graph.find_edge(old_parent, child) {
             self.graph.remove_edge(edge);
 
-            self.graph.add_edge(new_parent, child, ());
+            self.graph.add_edge(new_parent, child, SceneEdge {});
 
             Ok(())
         } else {
@@ -65,14 +80,14 @@ impl SceneGraph {
         }
     }
 
-    pub fn recursive_remove(&mut self, node: NodeIndex<Ix>) -> SceneResult<()> {
+    pub fn recursive_remove(&mut self, _node: NodeIndex<Ix>) -> SceneResult<()> {
         //TODO
         Ok(())
     }
 }
 
 impl Deref for SceneGraph {
-    type Target = StableDiGraph<Entity, (), Ix>;
+    type Target = StableDiGraph<SceneNode, SceneEdge, Ix>;
 
     #[inline(always)]
     fn deref(&self) -> &Self::Target { &self.graph }
--- combustion_scene/src/lib.rs
+++ combustion_scene/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate petgraph;
 extern crate fnv;
 extern crate specs;
+extern crate typemap;
 
 #[macro_use]
 extern crate combustion_common as common;
@@ -8,4 +9,5 @@ extern crate combustion_events as events;
 extern crate combustion_ecs as ecs;
 
 pub mod error;
+pub mod node;
 pub mod graph;
