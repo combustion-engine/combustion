5b958e8df79437e485ef401c670f8d9d662e4c16
Aaron Trent <novacrazy@gmail.com>
2017-01-03 16:16:38 -0600

    Improve formatting and functionality of the backtrace system
    Former-commit-id: 5507c749b3e76bc95efae79a751971afa0f22655

--- combustion_common/src/bt.rs
+++ combustion_common/src/bt.rs
@@ -1,9 +1,11 @@
 use std::os::raw::c_void;
 use std::path::Path;
+use std::thread;
 
-pub use backtrace::{trace, resolve, SymbolName, Backtrace};
+use backtrace::{trace, resolve, SymbolName};
 
 pub trait BacktraceFmt {
+    /// Formats backtrace symbol components in some way
     fn format(count: u32,
               name: Option<SymbolName>,
               addr: Option<*mut c_void>,
@@ -11,50 +13,88 @@ pub trait BacktraceFmt {
               lineno: Option<u32>) -> String;
 }
 
+/// Default backtrace formatter that tries to resemble rustc panic backtraces somewhat
 pub struct DefaultBacktraceFmt;
 
 impl BacktraceFmt for DefaultBacktraceFmt {
     fn format(count: u32,
               name: Option<SymbolName>,
               addr: Option<*mut c_void>,
-              _filename: Option<&Path>,
-              _lineno: Option<u32>) -> String {
+              filename: Option<&Path>,
+              lineno: Option<u32>) -> String {
         let name = name.and_then(|name| { name.as_str() }).unwrap_or("???");
 
-        format!("{:>5}: {:>20p} - {:<}\n", count, addr.unwrap_or(0x0 as *mut _), name)
+        lazy_static! {
+            static ref PADDING: String = format!("{:<24}", "");
+        }
+
+        let begin = format!("{:>4}: {:>18p} - {:<}\n{}", count, addr.unwrap_or(0x0 as *mut _), name, *PADDING);
+
+        let end = if let Some(filename) = filename {
+            format!("at {}:{}\n", filename.display(), lineno.unwrap_or(0))
+        } else {
+            format!("at <anonymous>:{}\n", lineno.unwrap_or(0))
+        };
+
+        begin + end.as_str()
     }
 }
 
-pub fn format_trace<Fmt: BacktraceFmt>(start: String) -> String {
-    // Ignore `format_trace` and `backtrace::trace` calls
+/// Generates a formatted backtrace (via `Fmt` type) from here, but expects `line` and `file` to be where it was called from.
+///
+/// The actual call to `format_trace` and `trace` are ignored.
+#[inline(never)]
+pub fn format_trace<Fmt: BacktraceFmt>(line: u32, file: &str) -> String {
+    // Ignore `format_trace` and `backtrace::trace` call, both of which are marked as #[inline(never)],
+    // so they will always show up.
     const IGNORE_COUNT: u32 = 2;
 
-    let mut traces = start;
+    let mut traces = format!("Stack backtrace for task \"<{}>\" at line {} of \"{}\":\n",
+                             thread::current().name().unwrap_or("unnamed"), line, file);
 
     let mut count = 0;
 
     trace(|frame| {
-        count += 1;
-
         if count <= IGNORE_COUNT {
-            return true;
+            count += 1;
+        } else {
+            let before = count;
+
+            resolve(frame.ip(), |symbol| {
+                traces += Fmt::format(count - IGNORE_COUNT,
+                                      symbol.name(),
+                                      symbol.addr(),
+                                      symbol.filename(),
+                                      symbol.lineno()).as_str();
+
+                count += 1;
+            });
+
+            // These will be equal if `resolve_cb` was not invoked
+            if count == before {
+                // If `symbol_address` doesn't work, oh well.
+                resolve(frame.symbol_address(), |symbol| {
+                    traces += Fmt::format(count - IGNORE_COUNT,
+                                          symbol.name(),
+                                          symbol.addr(),
+                                          symbol.filename(),
+                                          symbol.lineno()).as_str();
+
+                    count += 1;
+                });
+            }
         }
 
-        resolve(frame.ip(), |symbol| {
-            traces += Fmt::format(count - IGNORE_COUNT,
-                                  symbol.name(),
-                                  symbol.addr(),
-                                  symbol.filename(),
-                                  symbol.lineno()).as_str();
-        });
-
+        // Always continue
         true
     });
 
     traces
 }
 
-/// Returns a string containing the backtrace and a header message
+/// Returns a string containing the formatted backtrace and a header message
+///
+/// Pass a custom `BacktraceFmt` type to the macro to use custom formatting
 #[macro_export]
 macro_rules! backtrace {
     () => {
@@ -62,12 +102,7 @@ macro_rules! backtrace {
     };
 
     ($fmt:ty) => {
-        $crate::bt::format_trace::<$fmt>(
-            format!("Stack backtrace for task \"<{}>\" at Line {} of \"{}\":\n",
-            ::std::thread::current().name().unwrap_or("unnamed"),
-            line!(),
-            file!())
-        )
+        $crate::bt::format_trace::<$fmt>(line!(), file!())
     };
 }
 
