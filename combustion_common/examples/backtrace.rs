f6af6ffe3213bd7477b99b7526ac7a7ad509c715
Aaron Trent <novacrazy@gmail.com>
2017-01-06 14:08:32 -0600

    Update backtrace example
    Former-commit-id: 68dc4b359cbabac53f73fed8b76b361608ab8089

--- combustion_common/examples/backtrace.rs
+++ combustion_common/examples/backtrace.rs
@@ -1,14 +1,50 @@
 #[macro_use]
 extern crate combustion_common as common;
 
+use common::error::*;
+
+use std::io::{self, ErrorKind};
+
+fn from_error() -> Result<(), io::Error> {
+    Err(io::ErrorKind::AddrInUse.into())
+}
+
+fn rethrow2() -> TraceResult<(), io::Error> {
+    try_throw!(from_error());
+
+    Ok(())
+}
+
+fn errorable() -> TraceResult<i32, io::Error> {
+    throw!(ErrorKind::AlreadyExists);
+
+    Ok(43)
+}
+
+fn rethrow() -> TraceResult<i32, io::Error> {
+    let res = try_rethrow!(errorable());
+
+    println!("{}", res);
+
+    Ok(res)
+}
+
 #[inline(never)]
 fn test2() {
-    println!("{}", backtrace!());
+    if let Err(err) = rethrow() {
+        println!("{:?}", err);
+    }
+
+    if let Err(err) = rethrow2() {
+        println!("{:?}", err);
+    }
 }
 
 #[inline(never)]
 fn test() {
     test2();
+
+    println!("{}", backtrace!());
 }
 
 #[inline(never)]
