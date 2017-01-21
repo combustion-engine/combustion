1ab8bd6f7fb8d98003520e2607b3ff52dbd8d11f
Aaron Trent <novacrazy@gmail.com>
2017-01-21 17:18:35 -0600

    Use my parallel-event-emitter crate for events
    Former-commit-id: cb3e69e62a0551cbc0e1ee2576d18b065256453d

--- combustion_events/src/lib.rs
+++ combustion_events/src/lib.rs
@@ -1,16 +1,11 @@
 //! Event handling
 
-#![feature(conservative_impl_trait)]
 #![deny(missing_docs)]
-
-extern crate fnv;
-
-#[cfg(feature = "parallel")]
-extern crate futures;
-#[cfg(feature = "parallel")]
-extern crate futures_cpupool;
 
 #[macro_use]
 extern crate combustion_common as common;
 
-pub mod event_emitter;
+pub extern crate parallel_event_emitter;
+
+/// Use my Parallel Event Emitter crate
+pub use parallel_event_emitter::*;
