37d45743b5e3f23e8a94d1e67bef2d7a9379b0c7
Aaron Trent <novacrazy@gmail.com>
2017-02-01 19:23:35 -0600

    Indicate storage medium to Asset load/save methods

--- combustion_asset/src/traits/asset.rs
+++ combustion_asset/src/traits/asset.rs
@@ -1,11 +1,17 @@
 use std::io::prelude::*;
+use std::path::Path;
 
 use ::error::AssetResult;
 
-pub trait Asset where Self: Sized {
-    type LoadArgs: Clone;
-    type SaveArgs: Clone;
+pub enum AssetMedium<'a> {
+    File(&'a Path),
+    Memory,
+}
 
-    fn load<R: Read>(reader: R, args: Self::LoadArgs) -> AssetResult<Self>;
-    fn save<W: Write>(&self, writer: W, args: Self::SaveArgs) -> AssetResult<()>;
+pub trait Asset<'a> where Self: Sized {
+    type LoadArgs: Clone + 'a;
+    type SaveArgs: Clone + 'a;
+
+    fn load<R: BufRead + Seek>(reader: R, medium: AssetMedium<'a>, args: Self::LoadArgs) -> AssetResult<Self>;
+    fn save<W: Write>(&self, writer: W, medium: AssetMedium<'a>, args: Self::SaveArgs) -> AssetResult<()>;
 }
