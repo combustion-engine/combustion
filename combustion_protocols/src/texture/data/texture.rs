587f0fc71e979b52968b981517de16f06bea5937
Aaron Trent <novacrazy@gmail.com>
2017-02-02 21:49:27 -0600

    Use Blob in Texture and add WIP documentation

--- combustion_protocols/src/texture/data/texture.rs
+++ combustion_protocols/src/texture/data/texture.rs
@@ -1,14 +1,22 @@
+//! Data structures for manipulating textures
+
 use ::texture::protocol::TextureKind;
+use ::blob::Blob;
 
 use super::format::SpecificFormat;
 
+/// Represents the variations of textures that can be used
 #[derive(Clone, Serialize, Deserialize)]
 pub enum RootTexture {
+    /// Cubemap variant
     Cubemap(Cubemap),
+    /// Single texture variant
     Single(Texture),
+    /// Array of textures variant
     Array(Vec<Texture>),
 }
 
+/// Texture dimensions
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct Dimensions {
     /// Texture width
@@ -20,15 +28,17 @@ pub struct Dimensions {
 }
 
 impl Dimensions {
+    /// Constructor for `Dimensions` structure
     pub fn new(width: u32, height: u32, depth: u32) -> Dimensions {
         Dimensions { width: width, height: height, depth: depth }
     }
 }
 
+/// Represents a single texture
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Texture {
     /// Binary texture data
-    pub data: Vec<u8>,
+    pub data: Blob,
     /// Texture dimensions
     pub dimensions: Dimensions,
     /// Texture kind
@@ -37,12 +47,19 @@ pub struct Texture {
     pub format: SpecificFormat,
 }
 
+/// Represents a cubemap made of six unique textures
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Cubemap {
+    /// Right texture
     pub right: Texture,
+    /// Left texture
     pub left: Texture,
+    /// Top texture
     pub top: Texture,
+    /// Bottom texture
     pub bottom: Texture,
+    /// Back texture
     pub back: Texture,
+    /// Front texture
     pub front: Texture,
 }
