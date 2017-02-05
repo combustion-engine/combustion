7d42952779ea5c9a26e77d7720a8c04642e6cf12
Aaron Trent <novacrazy@gmail.com>
2017-02-05 04:23:27 -0600

    Commit WIP model loading stuff

--- combustion_asset/src/assets/model.rs
+++ combustion_asset/src/assets/model.rs
@@ -3,6 +3,8 @@
 use std::io::prelude::*;
 use std::ops::{Deref, DerefMut};
 use std::ascii::AsciiExt;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 use capnp::serialize_packed;
 use capnp::message::ReaderOptions;
@@ -12,27 +14,60 @@ use protocols::model::{protocol, EXTENSION};
 use protocols::model::data::{Model, Node};
 use protocols::model::storage::ModelSaveArgs;
 
+use assimp::{self, Scene};
+
+use ::cache::AssetHashMapCache;
 use ::error::{AssetResult, AssetError};
 use ::asset::{Asset, AssetMedium, AssetQuery};
+
+pub type AssimpSceneCache<'a> = AssetHashMapCache<'a, String, Scene<'a>>;
+
+#[derive(Debug, Clone, Copy)]
+pub enum ModelAssetQuery<'a> {
+    SupportedExtension(&'a str),
+}
+
+impl<'a> AssetQuery for ModelAssetQuery<'a> {
+    type Arguments = ModelAssetQuery<'a>;
+    type Result = bool;
+}
+
+#[derive(Clone)]
+pub struct ModelLoadArgs<'a> {
+    pub scene_cache: Rc<RefCell<AssimpSceneCache<'a>>>,
+}
 
 /// Model Asset
 pub struct ModelAsset(Model);
 
 impl<'a> Asset<'a> for ModelAsset {
-    type LoadArgs = ();
+    type LoadArgs = ModelLoadArgs<'a>;
     type SaveArgs = ();
 
-    type Query = ();
+    type Query = ModelAssetQuery<'a>;
 
-    fn query(_: ()) -> AssetResult<()> {
-        unimplemented!()
+    fn query(query: ModelAssetQuery<'a>) -> AssetResult<bool> {
+        match query {
+            ModelAssetQuery::SupportedExtension(ext) => {
+                Ok(ext == EXTENSION || assimp::formats::IMPORT_EXTENSIONS.contains_key(ext))
+            },
+            _ => unimplemented!()
+        }
     }
 
-    fn load<R: BufRead + Seek>(reader: R, medium: AssetMedium<'a>, _: ()) -> AssetResult<ModelAsset> {
-        unimplemented!()
+    fn load<R: BufRead + Seek>(reader: R, medium: AssetMedium<'a>, mut args: ModelLoadArgs<'a>) -> AssetResult<ModelAsset> {
+        if let AssetMedium::File(path) = medium {
+            if let Some(ext) = path.extension() {
+                let ext = try_throw!(ext.to_str().ok_or(AssetError::InvalidValue)).to_ascii_lowercase();
+
+                if ext == EXTENSION {} else {}
+            }
+        }
+
+        throw!(AssetError::UnsupportedMedium)
     }
 
-    fn save<W: Write>(&self, writer: W, medium: AssetMedium<'a>, _: ()) -> AssetResult<()> {
+    fn save<W: Write>(&self, _writer: W, _medium: AssetMedium<'a>, _: ()) -> AssetResult<()> {
         unimplemented!()
     }
 }
