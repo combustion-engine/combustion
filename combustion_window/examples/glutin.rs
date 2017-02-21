865032d6340b5fdb86f74c470349d392e0003791
Aaron Trent <novacrazy@gmail.com>
2017-02-20 19:41:59 -0600

    Add Glutin example

---
+++ combustion_window/examples/glutin.rs
@@ -0,0 +1,36 @@
+extern crate combustion_window;
+
+use combustion_window::provider::{WindowBuilder, WindowProvider};
+use combustion_window::providers::glutin::{glutin, GlutinWindowBuilder};
+
+fn main() {
+    let window =
+        GlutinWindowBuilder::new()
+            .size(800, 600)
+            .with_raw(|raw| {
+                raw.with_decorations(true)
+                   .with_visibility(true)
+            })
+            .build().unwrap();
+
+
+    unsafe { window.make_current(); }
+
+    'events: loop {
+        for event in window.poll_events() {
+            match event {
+                glutin::Event::Closed => {
+                    break 'events;
+                }
+                // process events here
+                _ => ()
+            }
+        }
+
+        // draw everything here
+
+        window.swap_buffers();
+
+        ::std::thread::sleep_ms(100);
+    }
+}
