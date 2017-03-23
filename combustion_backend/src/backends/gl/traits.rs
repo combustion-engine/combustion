945a49747bb154928871f3cf8d606261b4b1c428
Aaron Trent <novacrazy@gmail.com>
2017-03-23 12:57:10 -0500

    Document traits

--- combustion_backend/src/backends/gl/traits.rs
+++ combustion_backend/src/backends/gl/traits.rs
@@ -1,11 +1,16 @@
 use super::wrapper::types;
 use super::wrapper::error::*;
 
+/// Defines common routines for OpenGL objects
 pub trait GLObject {
+    /// Get the raw OpenGL identifier
     fn raw(&self) -> types::GLuint;
+    /// Consume self and return the raw OpenGL identifier
     fn into_raw(self) -> types::GLuint;
+    /// Check if the current object is a valid OpenGL object
     fn is_valid(&self) -> bool;
 
+    /// Return a `Result` representing if the current object is a valid OpenGL object
     fn check(&self) -> GLResult<()> {
         if self.is_valid() { Ok(()) } else {
             throw!(GLError::InvalidValue)
@@ -13,6 +18,8 @@ pub trait GLObject {
     }
 }
 
+/// Defines an object which can be made active in the OpenGL context
 pub trait GLBindable {
+    /// Bind and make active the current object
     fn bind(&self) -> GLResult<()>;
 }
