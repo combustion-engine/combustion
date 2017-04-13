d76376b975fbba42d9fe314cfa5aad6e0297a7f8
Aaron Trent <novacrazy@gmail.com>
2017-04-13 08:52:57 -0500

    Commit missing files

---
+++ combustion_log/src/lib.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+
+#[macro_use]
+extern crate slog_scope;
+extern crate slog_atomic;
+
+extern crate tinyfiledialogs;
+
+pub mod log;
+pub mod ext;
