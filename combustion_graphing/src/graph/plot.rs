a170cec9eb76f3a05f27c630239fb2e6b067d352
Aaron Trent <novacrazy@gmail.com>
2017-04-20 21:25:37 -0500

    Use more logical value for an alpha limit when calculating the gaussian dot PDF

--- combustion_graphing/src/graph/plot.rs
+++ combustion_graphing/src/graph/plot.rs
@@ -1,5 +1,7 @@
 use ::stat::gaussian_dot_pdf;
 
+const ALPHA_LIMIT: f64 = 1.0 / 255.0;
+
 pub fn plot_gaussian_dot<P>(x: i64, y: i64, alpha: f64, width: f64, hardness: f64, mut plot: P) where P: FnMut(i64, i64, f64) {
     let mut x0: i64 = 0;
 
@@ -24,12 +26,12 @@ pub fn plot_gaussian_dot<P>(x: i64, y: i64, alpha: f64, width: f64, hardness: f6
                 plot(x - x0, y - y0, f * alpha);
             }
 
-            if f < 0.001 { break; }
+            if f < ALPHA_LIMIT { break; }
 
             y0 += 1;
         }
 
-        if gaussian_dot_pdf(x0 as f64, width, hardness) < 0.001 { break; }
+        if gaussian_dot_pdf(x0 as f64, width, hardness) < ALPHA_LIMIT { break; }
 
         x0 += 1;
     }
