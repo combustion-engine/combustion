06ed72f244ff7c1d6f8aa396e71331f01ea90f44
Aaron Trent <novacrazy@gmail.com>
2017-04-21 18:30:32 -0500

    Divide width by half in the Gaussian function PDF

--- combustion_graphing/src/stat.rs
+++ combustion_graphing/src/stat.rs
@@ -1,5 +1,5 @@
 //! Probability Distribution Functions
 
 pub fn gaussian_dot_pdf(x: f64, width: f64, hardness: f64) -> f64 {
-    ::std::f64::consts::E.powf(-((x / width).powf(2.0f64.powf(hardness))))
+    ::std::f64::consts::E.powf(-((x / (width * 0.5)).powf(2.0f64.powf(hardness))))
 }
